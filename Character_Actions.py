8dfbc3f0e9083b7c4b077f81823ef213f7537605
Max Hightower <110921809+maxhightower@users.noreply.github.com>
2024-05-13 19:46:25 -0500

    go x13

--- Character_Actions.py
+++ Character_Actions.py
@@ -108,9 +108,9 @@ def Enact_Attack(Actor,Target,Weapon,Combat_Situation):
 
   #for i in Actor.Effects['Self_Attacking']['Rolling'][Weapon.Type]:
     # check if any effects can be added
-  damage_roll = Dice_Rolls.Damage_Roll(Weapon.Dice_Type,Weapon.Dice_Num,Actor,Target,Weapon,Establishing_Hierarchy.Attack_Score(Actor),Weapon.Damage_Type)
+  #damage_roll = Dice_Rolls.Damage_Roll(Weapon.Dice_Type,Weapon.Dice_Num,Actor,Target,Weapon,Establishing_Hierarchy.Attack_Score(Actor),Weapon.Damage_Type)
   # use the function Convert_Roll_to_Int to convert the roll x to an integer
-  x = Dice_Rolls.Roll_Dice(damage_roll.Dice_Type,damage_roll.Dice_Num)
+  damage_roll = Dice_Rolls.Roll_Dice(Weapon.Dice_Type,Weapon.Dice_Num)
   y = Attack_Modifier
 
 
