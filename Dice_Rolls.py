aee76c14395e1486758ed8af9703dc79b0792b72
Max Hightower <110921809+maxhightower@users.noreply.github.com>
2024-05-13 20:21:39 -0500

    go x15

--- Dice_Rolls.py
+++ Dice_Rolls.py
@@ -9,7 +9,7 @@ import numpy as np
 def Roll_Dice(Number_of_Dice,Dice_Type):
   roll_results = 0
   for i in range(Number_of_Dice):
-    roll_results = roll_results + randrange(1,Dice_Type)
+    roll_results = roll_results + randrange(1,int(Dice_Type))
   return roll_results
 
 
