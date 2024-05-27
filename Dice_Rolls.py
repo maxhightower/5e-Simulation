96043a8df69240ee1c430851aadb82c731d05a05
Max Hightower <110921809+maxhightower@users.noreply.github.com>
2024-05-27 09:50:12 -0500

    y

--- Dice_Rolls.py
+++ Dice_Rolls.py
@@ -340,9 +340,9 @@ class Natural_Weapon(Attack):
 
 def Choose_Target_Offense(Actor,Combat_Situation):
   # randomly choose a creature in the list Combat_Situation that isn't Actor
-  # options is Combat_Situation without Actor
+  # options is the list Combat_Situation without Actor
   options = Combat_Situation.remove(Actor)
-  target = random.choice(options)
+  target = np.random.choice(options)
   return target
 
 def Choose_Target_Support(Actor,Combat_Situation):
