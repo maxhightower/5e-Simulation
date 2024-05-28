c9d158e1d1b217a1c205fe716e2f2794e7eda982
Max Hightower <110921809+maxhightower@users.noreply.github.com>
2024-05-28 09:40:16 -0500

    y

--- Character_Actions.py
+++ Character_Actions.py
@@ -317,7 +317,7 @@ def Enact_Attack(Actor,Target,Weapon,combat_situation,new_combat_log):
     combat_round += 1
   
   # the last row of the combat_log_new is the current state of the combat situation
-  Target.Current_HP = new_combat_log.iloc[-1][Target.Name + ' Current_HP'] - damage
+  Target.Current_HP = Target.Current_HP - damage
   
   action_number = 'Undetermined'
   
